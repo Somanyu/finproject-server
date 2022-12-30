e84194da1283cb56b1634c3ee36417afcb685a11
Somanyu <somanyu.03samal@gmail.com>
2022-12-30 12:35:30 +0530

    🚧 Send and Receive message methods.

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
+
 const app = express();
 
 dotenv.config({ path: './.env' });
 
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.status(200).json({name: "Somanyu Samal", message: "This is API res from /"})
+    // Access the form value that was saved in the app's context
+    res.status(200).json({ from: app.locals.from, body: app.locals.body })
+
+    const accountSID = process.env.TWILIO_ACCOUNT_SID
+    const authToken = process.env.TWILIO_AUTH_TOKEN
+    const client = require("twilio")(accountSID, authToken)
+
+    // Create a message instance
+    client.messages.create({
+        from: 'whatsapp:+14155238886',
+        body: 'Hello there!',
+        to: 'whatsapp:+916370112909',
+    }).then(message => console.log(message.sid))
+})
+
+app.post('/sender-reply', (req, res) => {
+    console.log(`From: ${req.body.From}`);
+    console.log(`Message: ${req.body.Body}`);
+
+    // Save the value to a variable in the app's context
+    app.locals.from = req.body.From
+    app.locals.body = req.body.Body
 })
 
 app.listen(PORT, () => {
