4636d091d758eb14394b5a48174cd147a4b8a28c
Somanyu <somanyu.03samal@gmail.com>
2023-01-02 11:33:05 +0530

    ♻️ refactor: moved to routes express.Router()

--- index.js
+++ index.js
@@ -11,30 +11,33 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    // Access the form value that was saved in the app's context
-    res.status(200).json({ from: app.locals.from, body: app.locals.body })
+const messageRouter = require('./routes/message');
+app.use('/message', messageRouter);
 
-    const accountSID = process.env.TWILIO_ACCOUNT_SID
-    const authToken = process.env.TWILIO_AUTH_TOKEN
-    const client = require("twilio")(accountSID, authToken)
+// app.get('/', (req, res) => {
+//     // Access the form value that was saved in the app's context
+//     res.status(200).json({ from: app.locals.from, body: app.locals.body })
 
-    // Create a message instance
-    client.messages.create({
-        from: 'whatsapp:+14155238886',
-        body: 'Hello there!',
-        to: 'whatsapp:+916370112909',
-    }).then(message => console.log(message.sid))
-})
+//     const accountSID = process.env.TWILIO_ACCOUNT_SID
+//     const authToken = process.env.TWILIO_AUTH_TOKEN
+//     const client = require("twilio")(accountSID, authToken)
 
-app.post('/sender-reply', (req, res) => {
-    console.log(`From: ${req.body.From}`);
-    console.log(`Message: ${req.body.Body}`);
+//     // Create a message instance
+//     client.messages.create({
+//         from: 'whatsapp:+14155238886',
+//         body: 'Hello there!',
+//         to: 'whatsapp:+916370112909',
+//     }).then(message => console.log(message.sid))
+// })
 
-    // Save the value to a variable in the app's context
-    app.locals.from = req.body.From
-    app.locals.body = req.body.Body
-})
+// app.post('/sender-reply', (req, res) => {
+//     console.log(`From: ${req.body.From}`);
+//     console.log(`Message: ${req.body.Body}`);
+
+//     // Save the value to a variable in the app's context
+//     app.locals.from = req.body.From
+//     app.locals.body = req.body.Body
+// })
 
 app.listen(PORT, () => {
     console.log(`App listening on at http://localhost:${PORT}`);
---
+++ routes/message.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    // Access the form value that was saved in the response's context.
+    res.status(200).json({ from: req.app.locals.from, body: req.app.locals.body })
+});
+
+router.get('/send', (req, res) => {
+
+    const accountSID = process.env.TWILIO_ACCOUNT_SID
+    const authToken = process.env.TWILIO_AUTH_TOKEN
+    const client = require("twilio")(accountSID, authToken)
+
+    // Create a message instance.
+    client.messages.create({
+        from: 'whatsapp:+14155238886',
+        body: 'Hello there!',
+        to: 'whatsapp:+916370112909',
+    }).then(message => console.log(message.sid))
+
+})
+
+// https://230c-2405-201-a009-aa-ecee-a557-bc14-757e.ngrok.io/message/receive
+router.post('/receive', (req, res) => {
+    const from = req.body.From;
+    const body = req.body.Body;
+
+    console.log(`From: ${from}`);
+    console.log(`Message: ${body}`);
+
+    // Save the value to a variable in the response's context.
+    req.app.locals.from = from
+    req.app.locals.body = body
+})
+
+module.exports = router
