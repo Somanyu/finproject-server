3040f865665d7f2b374eeb3318223f0a46d4c764
Somanyu <somanyu.03samal@gmail.com>
2023-02-25 09:00:03 +0530

    🐛 Methods still fixing

--- routes/auth.js
+++ routes/auth.js
@@ -9,15 +9,15 @@ router.post('/signin', authController.signIn);
 // router.get('/logout', authController.logout);
 
 // Set the "Allow" header to include POST for "/signup" route
-router.options('/signup', (req, res) => {
-    res.header('Allow', 'GET, POST, OPTIONS');
-    res.send();
-});
+// router.options('/signup', (req, res) => {
+//     res.header('Allow', 'GET, POST, OPTIONS');
+//     res.send();
+// });
 
-// Set the "Allow" header to include POST for "/signin" route
-router.options('/signin', (req, res) => {
-    res.header('Allow', 'GET, POST, OPTIONS');
-    res.send();
-});
+// // Set the "Allow" header to include POST for "/signin" route
+// router.options('/signin', (req, res) => {
+//     res.header('Allow', 'GET, POST, OPTIONS');
+//     res.send();
+// });
 
 module.exports = router
